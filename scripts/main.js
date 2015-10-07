d14b6adc9e79bffdcdf76066898fa650b1f97eb0
unknown <sclayt@TST7-SCLAYE6330.atusa.corp.anntaylor.com>
2015-10-06 21:24:09 -0400

    updated time in slide, login modal, etc

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
 
 	// this is API section
 	var venueLoc = $("some input from DOM");
-	var baseURL = "http://api.jambase.com/events?zipCode=95128&page=1&api_key=69cc7meyzhktnpfe5zn9ut76&o=json";
+	var baseURL = "http://api.jambase.com/events?zipCode=95128&page=1&api_key=kp6zhebmsv848gku5eqwdd32&o=json";
 
 	function successFunc( data ) {
 		console.log('this is returned data.Events:', data.Events);
@@ -16,10 +16,17 @@
 		for ( var i = 1; i < 4; i++){
 
 			var artistName = slideloop[i].Artists[0].Name,
-				showDate = slideloop[i].Date,
+				showDate = new Date( slideloop[i].Date ),
 				showVenueName = slideloop[i].Venue.Name,
 				showVenueAddress = slideloop[i].Venue.Address,
-				showTickets = slideloop[i].TicketUrl;
+				showTickets;
+				
+				if(slideloop[i].TicketUrl === ""){
+					showTickets = 'http://naynaysoul.com/upcoming-events';
+				}
+				else {
+					showTickets = slideloop[i].TicketUrl;
+				}
 
 			var displayName = $(".content-slider").append('<div class="slides slide-1"><div class="slides__img-container"><div class="boximage'+(i+0)+'"></div></div><div class="slides__text-container js-holder">' + showDate + '</div><div class="slides__text-container js-holder">' + artistName + '</div><div class="slides__text-container venue-name">' + showVenueName + '</div><div class="slides__text-container venue-addy">' + showVenueAddress + '</div><a href="' + showTickets + '" target="_blank"><span class="get-tix">GET TIX</span></a></div>');		
 
